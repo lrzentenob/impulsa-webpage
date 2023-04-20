6b144a24ab6082910987a166733274d0a5507640
Josman <josemanuek@hotmail.com>
2023-04-20 12:23:26 -0600

    header menu ok, with no color, just basic behaviour

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -4,7 +4,7 @@ export const Header  = ()=>{
 
     return (
         <header>
-            <h1>Logo</h1>
+            <h1 className='logo'>Logo</h1>
             <input type='checkbox' className='navtoggle' id='togglecheck'></input>
             <nav>
                 <ul>
@@ -12,11 +12,11 @@ export const Header  = ()=>{
                     <li> <a href='#'>Seguros</a></li>
                     <li> <a href='#'>Riesgos</a></li>
                     <li> <a href='#'>Consultoria</a></li>
-                    <li> <p>800 902 3456</p> </li>
+                    <li> <p>800 902 3456</p></li>
                 </ul>
             </nav>
             <label htmlFor='togglecheck' className='navtoggle-label'>
-                <span>x</span>
+                <span></span>
             </label>
         </header>
 
