388f2600017a8c0401082c414ad78d1586f13757
Josman <josemanuek@hotmail.com>
2023-05-19 13:51:02 -0600

    calculadora de fianza first version

--- src/pages/Fianzas/FianzasCotizacion.tsx
+++ src/pages/Fianzas/FianzasCotizacion.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Footer } from "../../components/Fianzas/Landing/Footer";
 import { Header } from "../../components/Fianzas/Landing/Header";
 import { Submenu } from "../../components/Fianzas/Landing/Submenu";
@@ -11,58 +11,195 @@ import { formatLocalCurrency } from "../../utils/CurrencyFormatter";
 export const FianzasCotizacion = () => {
     
 
+    const [viciosOcultos,setViciosOcultos] = useState<boolean>(false);
     const [amount, setAmount] = useState('');
 
-
-    const [valuesUpfront] = useState([0,10,15,20,25,30,35,40,45,50,55,60])
+    const valuesUpfront = [0,10,15,20,25,30,35,40,45,50,55,60]
     const [minUpfront] = useState(0);
     const [maxUpfront] = useState(valuesUpfront.length - 1);
     const [positionUpfront, setPositionUpfront] = useState(0);
     const [upfront, setUpfront] = useState(valuesUpfront[0]);
 
-    function onUpdateUpfront(e:any) {
-        setPositionUpfront(e.target.value);
-        setUpfront(valuesUpfront[e.target.value]);
-    }
-
-    const [valuesAccomplish] = useState([0,10,20,60])
+    const valuesAccomplish = [0,10,15,20,25,30,35,40,45,50,55,60]
     const [minAccomplish] = useState(0);
     const [maxAccomplish] = useState(valuesAccomplish.length - 1);
     const [positionAccomplish, setPositionAccomplish] = useState(0);
     const [accomplish, setAccomplish] = useState(valuesAccomplish[0]);
 
-    function onUpdateAccomplish(e:any) {
-        setPositionAccomplish(e.target.value);
-        setAccomplish(valuesAccomplish[e.target.value]);
-    }
+    const valuesBid = [0,10,15,20,25,30,35,40,45,50,55,60]
+    const [minBid] = useState(0);
+    const [maxBid] = useState(valuesBid.length - 1);
+    const [positionBid, setPositionBid] = useState(0);
+    const [bid, setBid] = useState(valuesBid[0]);
 
-    const [valuesTerm] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,40,50])
+    const valuesTerm = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,40,50]
     const [minTerm] = useState(0);
     const [maxTerm] = useState(valuesTerm.length - 1);
     const [positionTerm, setPositionTerm] = useState(0);
     const [term, setTerm] = useState(valuesTerm[0]);
 
+    const [tipoFianza, setTipoFianza] = useState<'anticipo'|'licitacion'>('anticipo');
+    const inputAmountRef = useRef<any>();
+
+    const [montoContrato, setMontoContrato] =  useState<number>(0);
+    // const [porcentajeAnticipo, setPorcentajeAnticipo] =  useState<number>(0);
+    // const [porcentajeCumplimiento, setPorcentajeCumplimiento] =  useState<number>(0);
+
+    const [montoAnticipo, setMontoAnticipo] =  useState<number>(0);
+    const [primaNetaAnticipo, setPrimaNetaAnticipo]  = useState<number>(0);
+    const [derechoAnticipo, setDerechoAnticipo] = useState<number>(0);
+    const [gastoExpAnticipo, setGastoExpAnticipo] = useState<number>(2300);
+    const [anticipoSubt, setAnticipoSubt] = useState<number>(0);
+    const [anticipoIva, setAnticipoIva] = useState<number>(0);
+    const [anticipoTotal, setAnticipoTotal] = useState<number>(0);
+
+    /**VICIOS OCULTOS = true */
+    const [montoCump, setMontoCump] = useState<number>(0);
+    const [primaNetaCump, setPrimaNetaCump] = useState<number>(0);
+    const [derechosCump, setDerechosCump] = useState<number>(0);
+    const [gastosExpCump, setGastosExpCump] = useState<number>(2300);
+    const [cumpSubtotal, setCumpSubtotal] = useState<number>(0);
+    const [cumpIva, setCumpIva] = useState<number>(0);
+    const [cumpTotal, setCumpTotal] = useState<number>(0);
+    
+    const [totalFianza, setTotalFianza] = useState<number>(0);
+
+/**
+Monto Licitacion
+Prima Neta Licitacion
+Derechos Licitacion
+Gastos Expedicion Anticipo
+Licitacion SubTotal
+Licitacion IVA
+Licitacion Total
+ */
+    const [montoLiciation, setMontoLicitacion ] = useState<number>(0)
+    const [primaNetaLic, setPrimaNetLic ] = useState<number>(0)
+    const [derechosLic, setDerechosLic ] = useState<number>(0)
+    const [gastosExpLic, setGastosExpLic ] = useState<number>(2300)
+    const [licSubtotal, setLicSubtotal ] = useState<number>(0)
+    const [licIva, setLicIva ] = useState<number>(0)
+    const [licTotal, setLicTotal ] = useState<number>(0)
+    
+
+
+    function onUpdateUpfront(e:any) {
+        setPositionUpfront(e.target.value);
+        setUpfront(valuesUpfront[e.target.value]);
+    }
+    
+    function onUpdateAccomplish(e:any) {
+        setPositionAccomplish(e.target.value);
+        setAccomplish(valuesAccomplish[e.target.value]);
+    }
+
     function onUpdateTerm(e:any) {
         setPositionTerm(e.target.value);
         setTerm(valuesTerm[e.target.value]);
     }
+    function onUpdateBid(e:any) {
+        setPositionBid(e.target.value);
+        setBid(valuesBid[e.target.value]);
+    }
 
     function onAmtInputChange (e:any) {
 
-        if(import.meta.env.MODE === "development"){
-            console.log(`devMode: apiUrl: ${import.meta.env.VITE_API_URL} ,moneyRegex: ${import.meta.env.VITE_MONEY_ENTRY_REGEX}`)
-        }
-        else{
-            console.log(`prodMode: apiUrl: ${import.meta.env.VITE_API_URL} ,moneyRegex: ${import.meta.env.VITE_MONEY_ENTRY_REGEX}`)
-        }
         const entryAmount = e.target.value
-        const amountRE = new RegExp(import.meta.env.VITE_MONEY_ENTRY_REGEX!);
+
+        const amountRE = new RegExp(import.meta.env.VITE_MONEY_ENTRY_REGEX);
         const amountMatch = entryAmount.match(amountRE);
         
         if( amountMatch || !entryAmount ){
             setAmount(entryAmount);
         }
     }
+
+    async function onViciosOcultosCheck (e:any) {
+        setViciosOcultos(e.target.checked)
+        setTerm(1);
+        recalculateForm();
+    }
+    async function onTipoFianzaChange (e:any){
+        setTipoFianza(e.target.value);
+        resetForm();
+    }
+
+    async function resetForm(){
+        setPositionUpfront(0);
+        setUpfront(valuesUpfront[0]);
+
+        setPositionTerm(0);
+        setTerm(valuesTerm[0]);
+
+        setPositionAccomplish(0);
+        setAccomplish(valuesAccomplish[0]);
+
+        setAmount('');
+        inputAmountRef.current.focus();
+    }
+
+
+    function recalculateForm () {
+
+        const MontoContrato = parseFloat(amount)
+        
+        setMontoContrato( MontoContrato );
+        const MontoAnticipo = MontoContrato * upfront / 100
+        setMontoAnticipo( MontoAnticipo );
+
+        let primaNetaTmp = MontoAnticipo * 0.12;
+        const PrimaNetaFinal = primaNetaTmp < 1700 ? 1700 : primaNetaTmp
+
+        setPrimaNetaAnticipo( PrimaNetaFinal );
+
+        const DerechosAnticipo = PrimaNetaFinal *0.035;
+        setDerechoAnticipo( DerechosAnticipo );
+        
+        const AnticipoSubtotal = PrimaNetaFinal + DerechosAnticipo + gastoExpAnticipo;
+        setAnticipoSubt(AnticipoSubtotal);
+        
+        setAnticipoIva( AnticipoSubtotal * 0.16);
+        setAnticipoTotal( AnticipoSubtotal * 1.16);
+
+        let MontoCumplimiento = MontoContrato * (accomplish /100)
+        MontoCumplimiento = MontoCumplimiento < 1700 ? 1700 : MontoCumplimiento;
+        setMontoCump(MontoCumplimiento);
+
+        let PrimaNetaCumplimento = viciosOcultos ? (MontoCumplimiento / 12)*(term + 12) : MontoCumplimiento * 0.12;
+
+        PrimaNetaCumplimento = PrimaNetaCumplimento < 1700 ? 1700 : PrimaNetaCumplimento;
+        setPrimaNetaCump(PrimaNetaCumplimento);
+
+        const DerechosCumplimiento = PrimaNetaCumplimento * 0.035;
+        setDerechosCump(DerechosCumplimiento);
+
+        const CumplimientoSubtotal = PrimaNetaCumplimento + DerechosCumplimiento + gastosExpCump;
+        setCumpSubtotal(CumplimientoSubtotal);
+        setCumpIva( CumplimientoSubtotal *0.16);
+        setCumpTotal( CumplimientoSubtotal *1.16);
+        setTotalFianza( CumplimientoSubtotal *1.16 + AnticipoSubtotal * 1.16  );
+
+
+        const MontoLicitacion = MontoContrato * bid / 100;
+        setMontoLicitacion(MontoLicitacion);
+
+        let PrimaNetaLicitacion =  MontoLicitacion * 0.03;
+        PrimaNetaLicitacion = PrimaNetaLicitacion < 1700 ? 1700 : PrimaNetaLicitacion;
+        setPrimaNetLic(PrimaNetaLicitacion);
+
+        const DerechosLicitacion = PrimaNetaLicitacion * 0.035;
+        setDerechosLic(DerechosLicitacion);
+
+        const LicitacionSubtotal = PrimaNetaLicitacion + DerechosLicitacion + gastosExpLic
+        setLicSubtotal( LicitacionSubtotal);
+        setLicIva( LicitacionSubtotal * 0.16);
+        setLicTotal( LicitacionSubtotal * 1.16);   
+    }
+
+    useEffect( ()=>{
+        recalculateForm();
+    },[amount,upfront,accomplish, term, bid])
+
 
     return (
         <>
@@ -80,44 +217,119 @@ export const FianzasCotizacion = () => {
 
                         <div className="user-input">
                             <div className="label">
-                                <p>Monto del Contrato: { amount ?formatLocalCurrency(parseFloat(amount),'$','','') : '' } </p>
+                                <p>Tipo fianza:</p>
                             </div>
-                            <input type="text" onChange={onAmtInputChange} value={amount}></input>
+                            <select value={tipoFianza} onChange={onTipoFianzaChange}>
+                                    <option value='anticipo'>Anticipo /Cumplimiento</option>
+                                    <option value='licitacion'>Licitacion/Concurso</option>
+                            </select>
+
                         </div>
 
-                        <div className="range-selection">
-                            <div className="label">
-                                <p>Anticipo <b>{upfront} %</b></p>
-                            </div>
-                            <RangeSlider min={minUpfront} max={maxUpfront} value={positionUpfront} onUpdateHandler={onUpdateUpfront} step={1} />                     
-                        </div>
-
-                        <div className="range-selection">
-                            <div className="label">
-                                <p>Cumplimiento <b>{accomplish} %</b></p>
-                            </div>
-                            <RangeSlider min={minAccomplish} max={maxAccomplish} value={positionAccomplish} onUpdateHandler={onUpdateAccomplish} step={1} />                     
-                        </div>
-
-                        <div className="user-input">
                         
+                        <div className="user-input">
                             <div className="label">
-                                <label htmlFor="vicios-ocultos-checkbox">Con vicios ocultos</label>
+                                <p>Monto del Contrato: { amount ?formatLocalCurrency(parseFloat(amount)) : '' } </p>
                             </div>
-                            <input type="checkbox" id='vicios-ocultos-checkbox'></input>
-
+                            <input type="text" onChange={onAmtInputChange} value={amount} ref={inputAmountRef}></input>
                         </div>
 
-                        <div className="range-selection">
-                            <div className="label">
-                                <p>Plazo de Ejecución de Obra en Meses <b>{term} Meses</b></p>
+
+                        { tipoFianza ==='anticipo' &&
+                        <div>
+                            <div className="range-selection">
+                                <div className="label">
+                                    <p>Anticipo <b>{upfront} %</b></p>
+                                </div>
+                                <RangeSlider min={minUpfront} max={maxUpfront} value={positionUpfront} onUpdateHandler={onUpdateUpfront} step={1} />                     
                             </div>
-                            <RangeSlider min={minTerm} max={maxTerm} value={positionTerm} onUpdateHandler={onUpdateTerm} step={1} />                     
-                        </div>
+
+                            <div className="range-selection">
+                                <div className="label">
+                                    <p>Cumplimiento <b>{accomplish} %</b></p>
+                                </div>
+                                <RangeSlider min={minAccomplish} max={maxAccomplish} value={positionAccomplish} onUpdateHandler={onUpdateAccomplish} step={1} />                     
+                            </div>
+
+                            <div className="user-input">
+                            
+                                <div className="label">
+                                    <label htmlFor="vicios-ocultos-checkbox"></label>
+                                </div>
+                                <label htmlFor="vicios-ocultos-checkbox">
+                                <input type="checkbox" id='vicios-ocultos-checkbox' checked={viciosOcultos} onChange={onViciosOcultosCheck}></input>
+                                &nbsp;&nbsp;Con vicios ocultos</label>
+
+
+                            </div>
+
+                            { viciosOcultos &&
+                            <div className="range-selection">
+                                    <div className="label">
+                                    <p>Plazo de Ejecución de Obra en Meses <b>{term} Meses</b></p>
+                                </div>
+                                <RangeSlider min={minTerm} max={maxTerm} value={positionTerm} onUpdateHandler={onUpdateTerm} step={1} />                     
+                            </div>}
+                        </div>}
+
+
+                        { tipoFianza ==='licitacion' &&
+                        <div>
+                            <div className="range-selection">
+                                <div className="label">
+                                    <p>Porcentaje Licitación <b>{bid} %</b></p>
+                                </div>
+                                <RangeSlider min={minBid} max={maxBid} value={positionBid} onUpdateHandler={onUpdateBid} step={1} />                     
+                            </div>
+
+                        </div>}
 
                     </div>
-                
-                    { amount &&
+                    {amount && tipoFianza === 'anticipo' &&
+                    <div className="scorecard">
+                            <h2>Fianza Anticipo/Cumplimiento Total: {formatLocalCurrency(totalFianza)}</h2>
+                            <hr></hr>
+                        <div>
+                            {/* <p>Monto Contrato: {formatLocalCurrency(montoContrato)}</p> */}
+                            <p>Monto Anticipo: {formatLocalCurrency(montoAnticipo)}</p>
+                            <p>Prima Neta Anticipo: {formatLocalCurrency(primaNetaAnticipo)}</p>
+                            <p>Derechos Anticipo: {formatLocalCurrency(derechoAnticipo)}</p>
+                            <p>Gastos Expedicion Anticipo: {formatLocalCurrency(gastoExpAnticipo)}</p>
+                            <p>Anticipo Subtotal: {formatLocalCurrency(anticipoSubt)}</p>
+                            <p>Anticipo IVA: {formatLocalCurrency(anticipoIva)}</p>
+                            <h3>Anticipo Total: {formatLocalCurrency(anticipoTotal)}</h3>
+                            <hr></hr>
+                        </div>
+                        <div>
+                            {/* <p>Monto Cumplimiento: {formatLocalCurrency(montoCump)}</p> */}
+                            <p>Prima Neta Cumplimiento: {formatLocalCurrency(primaNetaCump)}</p>
+                            <p>Derechos Cumplimiento: {formatLocalCurrency(derechosCump)}</p>
+                            <p>Gastos Exp Cumplimiento: {formatLocalCurrency(gastosExpCump)}</p>
+                            <p>Cumplimiento Sub: {formatLocalCurrency(cumpSubtotal)}</p>
+                            <p>Cumplimiento IVA: {formatLocalCurrency(cumpIva)}</p>
+                            <h3>Cumplimiento Total: {formatLocalCurrency(cumpTotal)}</h3>
+                            <hr></hr>                            
+                        </div>
+                    </div>}
+
+                    {amount && tipoFianza === 'licitacion' &&
+                    <div className="scorecard">
+                            <h2>Fianza Licitacion Total: {formatLocalCurrency(licTotal)}</h2>
+                            <hr></hr>
+                        <div>
+                            <p>Monto Licitacion: {formatLocalCurrency(montoLiciation)}</p>
+                            <p>Prima Neta Licitación: {formatLocalCurrency(primaNetaLic)}</p>
+                            <p>Derechos Licitación: {formatLocalCurrency(derechosLic)}</p>
+                            <p>Gastos Expedicion Licitación: {formatLocalCurrency(gastosExpLic)}</p>
+                            <p>Licitación Subtotal: {formatLocalCurrency(licSubtotal)}</p>
+                            <p>Licitación IVA: {formatLocalCurrency(licIva)}</p>
+                            <h3>Licitación Total: {formatLocalCurrency(licTotal)}</h3>
+                            <hr></hr>
+                        </div>
+
+                    </div>}
+
+                    { false &&
                     <table>
                         <tr>
                             <th className="th-left">Concepto</th>
@@ -126,12 +338,12 @@ export const FianzasCotizacion = () => {
                         </tr>
                         <tr>
                             <td data-cell="full"><b>Monto Fianza</b>    </td>
-                            <td data-cell="limited">{formatLocalCurrency(parseFloat(amount)*1000000*(upfront/100), "$","","")}</td>
-                            <td data-cell="civil">{formatLocalCurrency(parseFloat(amount)*1000000*(accomplish/100), "$","","")}</td>
+                            <td data-cell="limited">{formatLocalCurrency(parseFloat(amount)*(upfront/100))}</td>
+                            <td data-cell="civil">{formatLocalCurrency(parseFloat(amount)*(accomplish/100))}</td>
                         </tr>
                         <tr>
                             <td data-cell="full">Prima Neta</td>
-                            <td data-cell="limited">$3,600.00</td>
+                            <td data-cell="limited">{ }</td>
                             <td data-cell="civil">$3,400.00</td>                            
                         </tr>
                         <tr>
@@ -206,7 +418,7 @@ export const FianzasCotizacion = () => {
                */}
                 </div>
             </section>
-            {/* <Footer /> */}
+            <Footer />
         </>
     );
 }
--- src/utils/CurrencyFormatter.ts
+++ src/utils/CurrencyFormatter.ts
@@ -1,25 +1,17 @@
-export  const formatLocalCurrency = (numero:number, currencySy:string, fractionSy:string,decimalSy:string) => {
-  const formmatter = new Intl.NumberFormat("es-MX", {
-    style: "currency",
-    currency: "MXN",
+
+export const formatLocalCurrency = (numero: number) => {
+
+  const formmatter = new Intl.NumberFormat('es-MX', {
+      style: 'currency',
+      currency: 'MXN'
   });
 
-  const numberString = formmatter
-    .formatToParts(numero)
-    .map(({ type, value }) => {
-      
+  const numberString = formmatter.formatToParts(numero).map( ( { type, value } ) => {
       switch (type) {
-        case "currency":
-          return currencySy;
-        case "fraction":
-          return fractionSy;
-        case "decimal":
-          return decimalSy;
-        default:
-          return value;
+          case 'currency': return '$' ;
+          default: return value;
       }
-    })
-    .reduce((string, part) => string + part );
+  }).reduce((string, part) => string + part);
 
   return numberString;
-};
+}
