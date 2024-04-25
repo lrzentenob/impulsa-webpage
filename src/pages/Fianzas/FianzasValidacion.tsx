9345974e63124164f03d9f44888ec1a785e7a0e5
JorgeDMartinez <133905568+JorgeDMartinez@users.noreply.github.com>
2024-04-25 16:14:00 -0600

    Corrección de nombres de Afianzadoras

--- src/pages/Fianzas/FianzasValidacion.tsx
+++ src/pages/Fianzas/FianzasValidacion.tsx
@@ -33,23 +33,22 @@ export const FianzasValidacion = () => {
 
                     <div className="fianza-valid-brands">
                     <ul>
-                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://fianzas.aserta.com.mx/misFianzasInsurgentesIP17/ConsultaLineCaptura.jsp'>Afianzadora Insurgentes</Link></p></li>
-                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://fianzas.aserta.com.mx/misFianzasAsertaIP17/ConsultaLineCaptura.jsp'>Afianzadora Aserta</Link></p></li>
-                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://www.iconfianza.com.mx/iConfianza2014/validacion/ValLineaValidacion.aspx'>Afianzas Monterrey</Link></p></li>
+                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://fianzas.aserta.com.mx/misFianzasInsurgentesIP17/ConsultaLineCaptura.jsp'>Aseguradora Insurgentes</Link></p></li>
+                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://fianzas.aserta.com.mx/misFianzasAsertaIP17/ConsultaLineCaptura.jsp'>Aseguradora Aserta</Link></p></li>
+                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='http://apps.dorama.mx/validafianza/sitios/default.aspx?AspxAutoDetectCookieSupport=1'>Afianzadora Dorama</Link></p></li>
                         <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://serf.libertyfianzas.com/GSRFK03P/pf_indice.html1'>Liberty Fianzas</Link></p></li>
                         <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='http://apps.sofimex.com.mx/valida_sofimex/Sitios/'>Afianzadora Sofimex</Link></p></li>
                         <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://validafianza.berkleymex.com/ValidaFianzaBerkley/sitios/default.aspx'>Afianzadora Berkley</Link></p></li>
-                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='http://apps.dorama.mx/validafianza/sitios/default.aspx?AspxAutoDetectCookieSupport=1'>Fianzas Doroma</Link></p></li>
-                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://www.zurichfianzas.com.mx/ZIOF/(S(oabnyo32hbfdisucgdbruht0))/Electronicos/Ratificacion/Ratificacion2.aspx'>Zurich Fianzas de Mexico</Link></p></li>
-                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='http://187.188.115.53/aFi_validafianza/Sitios/Default.aspx'>Fiducia</Link></p></li>
+                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='http://187.188.115.53/aFi_validafianza/Sitios/Default.aspx'>Afianzadora Fiducia</Link></p></li>
+                        {/* <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://www.zurichfianzas.com.mx/ZIOF/(S(oabnyo32hbfdisucgdbruht0))/Electronicos/Ratificacion/Ratificacion2.aspx'>Zurich Fianzas de Mexico</Link></p></li> */}
+                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://www.iconfianza.com.mx/iConfianza2014/validacion/ValLineaValidacion.aspx'>Aseguradora Chubb Fianzas Monterrey</Link></p></li>
                         <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://fianzas.cescemex.mx:9090/(S(2vxjgvgwwovrxx1biv02v0nw))/Electronicos/Ratificacion/Ratificacion2.aspx'>Afianzadora Cescemex</Link> </p></li>
-                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://www.iconfianza.com.mx/iConfianza2014/validacion/ValLineaValidacion.aspx'>Chubb México</Link></p></li>
+                        {/* <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://www.iconfianza.com.mx/iConfianza2014/validacion/ValLineaValidacion.aspx'>Chubb México</Link></p></li> */}
                         <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://negocios.mapfre.com.mx/fianzas/ratificacion.aspx'>Afianzadora Mapfre</Link></p></li>
-                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://dynamic.tmhcc.com/validation/sitios/default.aspx'>Tokio Marine</Link></p></li>
+                        <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://dynamic.tmhcc.com/validation/sitios/default.aspx'>Afianzadora Tokio Marine</Link></p></li>
                         <li><p><FontAwesomeIcon icon={faCircleCheck} size='1x' className="icon" /> <Link to='https://www.fianzasatlas.net/efianza/Validador/ValidadorDocumentos.aspx'>Afianzadora Atlas</Link></p></li>
                     </ul>
                     </div>
-                    
                 </div>
             </section>
             <Footer />
